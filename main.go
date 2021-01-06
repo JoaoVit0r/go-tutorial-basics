758933bbb2b3981e66192cf111ad230c1d9e6ec2
joaocunha1 <joaocunha@alunos.utfpr.edu.br>
2021-01-06 12:00:23 -0300

    Step 1: using for and println

---
+++ main.go
@@ -0,0 +1,17 @@
+// package name
+package main
+
+// imported packages
+import "fmt"
+
+func main() {
+	// infinite loop
+	for {
+		fmt.Println("================================")
+		fmt.Println("(1) Fazer Buscas")
+		fmt.Println("(2) Exibir Historico")
+		fmt.Println("(0) Sair do Programa")
+
+		break
+	}
+}
