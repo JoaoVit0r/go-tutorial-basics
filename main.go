0f701ac02813a35783aff4c3304d59b8e6ef221d
joaocunha1 <joaocunha@alunos.utfpr.edu.br>
2021-01-07 11:40:39 -0300

    Step 2 - Using Scan and Switch. Working with errors

--- main.go
+++ main.go
@@ -7,11 +7,48 @@ import "fmt"
 func main() {
 	// infinite loop
 	for {
-		fmt.Println("================================")
+		fmt.Println("=================================")
 		fmt.Println("(1) Fazer Buscas")
 		fmt.Println("(2) Exibir Historico")
 		fmt.Println("(0) Sair do Programa")
 
-		break
+		choice, err := getChoice()
+
+		if err != nil {
+			fmt.Println("Error:", err.Error())
+			continue
+		}
+
+		// alternatively -> switch choice, _ := getChoice(); choice {
+		switch choice {
+		case 1:
+			fmt.Println("Iniciando Buscas...")
+			// startSearch()
+		case 2:
+			fmt.Println("Exibindo Histórico...")
+			// showHistory()
+		case 0:
+			fmt.Println("Saindo do programa")
+			// os.Exit(0)
+			return
+		default:
+			fmt.Println("Opção Inválida")
+		}
 	}
+}
+
+func getChoice() (int, error) {
+	var choice int
+
+	fmt.Print("	Escolha: ")
+
+	_, err := fmt.Scan(&choice)
+
+	if err != nil {
+		return -1, err
+	}
+
+	fmt.Println("__________________________________")
+
+	return choice, nil
 }
